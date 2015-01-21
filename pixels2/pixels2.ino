e095d6a53d080f48b40a03b70ac29e840a12ebaa
Paul Grayson <pdg@alum.mit.edu>
2015-01-20 17:31:25 -0800

    presentation and updated sketch

--- pixels2/pixels2.ino
+++ pixels2/pixels2.ino
@@ -9,28 +9,32 @@ void setup() {
   strip.show();
 }
 
+void setColor(uint8_t i, uint8_t b0, uint8_t b1) {
+  strip.setPixelColor(i, strip.Color((b1 >> 2) & 31, ((b1 << 3)+(b0>>5)) & 31, b0 & 31));
+}
+
 void nextByte(uint8_t b) {
   static uint8_t i = 0;
-  static uint8_t b1 = 0;
-  static uint8_t b1_set = 0;
+  static uint8_t b0 = 0;
+  static uint8_t b0_set = 0;
   
-  if(!b1_set)
+  if(!b0_set)
   {
-    b1 = b;
-    b1_set = 1;
+    b0 = b;
+    b0_set = 1;
     return;
   }
   
-  if(b1 == 0xff && b == 0xff)
+  if(b0 == 0xff && b == 0xff)
   {
     i = 0;
-    b1_set = 0;
+    b0_set = 0;
     return;
   }
   
-  strip.setPixelColor(i, strip.Color((b >> 2) & 31, ((b << 3)+(b1>>5)) & 31, b1 & 31));
+  setColor(i, b0, b);
 
-  b1_set = 0;
+  b0_set = 0;
   i ++;
   if(i == 40)
   {
