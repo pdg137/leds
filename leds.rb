10269e8b12c235830524b9bad898da6f46b8bd95
Paul Grayson <pdg@alum.mit.edu>
2015-01-21 16:45:25 -0800

    changed rgb order

--- leds.rb
+++ leds.rb
@@ -4,9 +4,11 @@ def rgb(r,g,b)
   OUTPUT.write([b + (g << 5) + (r << 10)].pack("S<"))
 end
 
-10.times do
-  rgb(0,0,31)
-  rgb(0,31,0)
+40.times do
   rgb(31,0,0)
+  rgb(0,31,0)
+  rgb(0,0,31)
   rgb(31,31,31)
 end
+
+sleep 0.25
